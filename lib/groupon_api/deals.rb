36681119894f965f4978bd7548dd25a87acf59f2
Bryan Liff <bliff@minerva-group.com>
2015-04-24 09:35:11 -0400

    Added type checking for default .deals params merge

--- lib/groupon_api/deals.rb
+++ lib/groupon_api/deals.rb
@@ -3,8 +3,8 @@ module GrouponApi
     raise ::ArgumentError, 'param :ts_token cannot be nil' if GrouponApi.config.ts_token.nil?
     raise ::ArgumentError, 'param :ts_token must match /US_AFF_0_\d+_212556_0/' unless GrouponApi.config.ts_token.match(API_KEY_FORMAT)
     
-    params.merge!(GrouponApi.config.deals)
     params.merge!(tsToken: GrouponApi.config.ts_token)
+    params.merge!(GrouponApi.config.deals) if GrouponApi.config.deals.kind_of?(Hash)
     
     GrouponApi::Request.call('deals', params)
   end
